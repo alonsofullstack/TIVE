97f9d223422f2bdd96bce16d6c6aedb585a22e76
Yoset H <alonsoyhc@gmail.com>
2026-05-27 23:44:11 -0500

    anv

--- src/services/cardGenerator.js
+++ src/services/cardGenerator.js
@@ -904,7 +904,7 @@ module.exports = function (bot) {
             userState.delete(chatId);
             await generarTIVE(chatId, prepared, null, sourceBuffer, { anv: 'TARJETA FISICA ADELANTE.pdf', rev: 'TARJETA FISICA ATRAS.pdf' }, {
                 noQR: true,
-                cropTopAnv: 35, cropBottomAnv: 35, cropLeftAnv: 1, cropRightAnv: 1,  // recorte ANVERSO
+                cropTopAnv: 1, cropBottomAnv: 1, cropLeftAnv: 1, cropRightAnv: 1,  // recorte ANVERSO
                 cropTopRev: 35, cropBottomRev: 35, cropLeftRev: 26, cropRightRev: 26   // recorte REVERSO
             });
             return;
